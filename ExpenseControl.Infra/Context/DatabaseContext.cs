de505b158c3120ca80f32586b783a751f1691942
Adriel <vitor.adriel@martinellilabs.com.br>
2023-09-10 17:42:36 -0300

    :tada: Initial Commit

---
+++ ExpenseControl.Infra/Context/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace ExpenseControl.Infra.Context
+{
+    public class DatabaseContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+        public DatabaseContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            var connectionString = _configuration.GetConnectionString("ExpenseControlDB");
+            builder.UseSqlServer(connectionString);
+        }
+    }
+}
