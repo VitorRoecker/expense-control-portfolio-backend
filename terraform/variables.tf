8eb4f6476bab80fa9a323f2d2c8a664485290d45
Adriel <vitoradrielroecker@gmail.com>
2024-07-02 19:30:52 -0300

    :rotating_light: Tests pipeline

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,56 +4,67 @@
 
 variable "database_port" {
   type        = number
+  default     = 1433
   description = "Database port (_e.g._ `3306` for `MySQL`). Used in the DB Security Group to allow access to the DB instance from the provided `security_group_ids`"
 }
 
 variable "deletion_protection" {
   type        = bool
+  default     = true
   description = "Set to true to enable deletion protection on the RDS instance"
 }
 
 variable "multi_az" {
   type        = bool
+  default     = false
   description = "Set to true if multi AZ deployment must be supported"
 }
 
 variable "storage_encrypted" {
   type        = bool
+  default     = false
   description = "(Optional) Specifies whether the DB instance is encrypted. The default is false if not specified"
 }
 
 variable "allocated_storage" {
   type        = number
+  default     = 1
   description = "The allocated storage in GBs"
 }
 
 variable "engine" {
   type        = string
+  default     = "sqlserver-se"
   description = "Database engine type"
 }
 
 variable "engine_version" {
   type        = string
+  default     = "14.0.3465.1"
   description = "Database engine version, depends on engine type"
 }
 
 variable "major_engine_version" {
   type        = string
+  default     = "14.0"
   description = "Database MAJOR engine version, depends on engine type"
 }
 
 variable "instance_class" {
   type        = string
+  default     = "db.t3.micro"
   description = "Class of RDS instance"
 }
 
 variable "db_parameter_group" {
   type        = string
+  default     = null
   description = "Parameter group, depends on DB engine used"
 }
 
 variable "apply_immediately" {
   type        = bool
+  default     = false
   description = "Specifies whether any database modifications are applied immediately, or during the next maintenance window"
 }
 
