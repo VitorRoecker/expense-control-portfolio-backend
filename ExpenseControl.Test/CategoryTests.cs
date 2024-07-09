f4d9088186234b17cf3ac2e5ed1c797c0bbf5ccb
Adriel <vitoradrielroecker@gmail.com>
2024-07-08 23:43:43 -0300

    :white_check_mark: Adding unit tests

---
+++ ExpenseControl.Test/CategoryTests.cs
@@ -0,0 +1,68 @@
+﻿using ExpenseControl.Domain.Entities;
+using ExpenseControl.Domain.Enumerables;
+using Xunit;
+
+namespace ExpenseControl.Test;
+public class CategoryTests
+{
+    [Fact]
+    public void CriarCategoria_EntradaValida_DeveCriarComSucesso()
+    {
+        // Arrange
+        var categoria = new Category
+        {
+            Name = "Categoria Teste",
+            Description = "Descrição da Categoria Teste",
+            Type = CategoryTypeEnum.Expense,
+            UserId = "usuarioteste"
+        };
+
+        // Act & Assert
+        Assert.NotNull(categoria);
+        Assert.Equal("Categoria Teste", categoria.Name);
+        Assert.Equal("Descrição da Categoria Teste", categoria.Description);
+        Assert.Equal(CategoryTypeEnum.Expense, categoria.Type);
+        Assert.Equal("usuarioteste", categoria.UserId);
+    }
+
+
+    [Fact]
+    public void AtualizarCategoria_EntradaValida_DeveAtualizarComSucesso()
+    {
+        // Arrange
+        var categoria = new Category
+        {
+            Name = "Categoria Teste",
+            Description = "Descrição da Categoria Teste",
+            Type = CategoryTypeEnum.Expense,
+            UserId = "usuarioteste"
+        };
+
+        // Act
+        categoria.Name = "Categoria Atualizada";
+        categoria.Description = "Descrição Atualizada";
+
+        // Assert
+        Assert.Equal("Categoria Atualizada", categoria.Name);
+        Assert.Equal("Descrição Atualizada", categoria.Description);
+    }
+
+    [Fact]
+    public void ExcluirCategoria_EntradaValida_DeveExcluirComSucesso()
+    {
+        // Arrange
+        var categoria = new Category
+        {
+            Name = "Categoria Teste",
+            Description = "Descrição da Categoria Teste",
+            Type = CategoryTypeEnum.Expense,
+            UserId = "usuarioteste"
+        };
+
+        // Act
+        categoria = null;
+
+        // Assert
+        Assert.Null(categoria);
+    }
+}
---
+++ ExpenseControl.Test/IncomeTests.cs
@@ -0,0 +1,135 @@
+﻿using ExpenseControl.Domain.Entities;
+using ExpenseControl.Domain.Enumerables;
+using Xunit;
+
+namespace ExpenseControl.Test
+{
+    public class IncomeTests
+    {
+        [Fact]
+        public void CriarReceitaFixa_EntradaValida_DeveCriarComSucesso()
+        {
+            // Arrange
+            var receita = new Income
+            {
+                Description = "Receita Teste",
+                Amount = 500.00m,
+                Type = TransactionTypeEnum.Fix,
+                CategoryId = Guid.NewGuid(),
+                EntryDate = DateTime.Now
+            };
+
+            // Act & Assert
+            Assert.NotNull(receita);
+            Assert.Equal("Receita Teste", receita.Description);
+            Assert.Equal(500.00m, receita.Amount);
+            Assert.Equal(TransactionTypeEnum.Fix, receita.Type);
+            Assert.Equal(DateTime.Now.Date, receita.EntryDate.Date); // Verifica se a data de entrada é hoje
+        }
+
+        [Fact]
+        public void CriarReceitaVariavel_EntradaValida_DeveCriarComSucesso()
+        {
+            // Arrange
+            var receita = new Income
+            {
+                Description = "Receita Teste",
+                Amount = 500.00m,
+                Type = TransactionTypeEnum.Variable,
+                CategoryId = Guid.NewGuid(),
+                EntryDate = DateTime.Now
+            };
+
+            // Act & Assert
+            Assert.NotNull(receita);
+            Assert.Equal("Receita Teste", receita.Description);
+            Assert.Equal(500.00m, receita.Amount);
+            Assert.Equal(TransactionTypeEnum.Variable, receita.Type);
+            Assert.Equal(DateTime.Now.Date, receita.EntryDate.Date); // Verifica se a data de entrada é hoje
+        }
+
+        [Fact]
+        public void AtualizarReceitaFixa_EntradaValida_DeveAtualizarComSucesso()
+        {
+            // Arrange
+            var receita = new Income
+            {
+                Description = "Receita Teste",
+                Amount = 500.00m,
+                Type = TransactionTypeEnum.Fix,
+                CategoryId = Guid.NewGuid(),
+                EntryDate = DateTime.Now
+            };
+
+            // Act
+            receita.Description = "Receita Atualizada";
+            receita.Amount = 600.00m;
+
+            // Assert
+            Assert.Equal("Receita Atualizada", receita.Description);
+            Assert.Equal(600.00m, receita.Amount);
+        }
+
+        [Fact]
+        public void AtualizarReceitaVariavel_EntradaValida_DeveAtualizarComSucesso()
+        {
+            // Arrange
+            var receita = new Income
+            {
+                Description = "Receita Teste",
+                Amount = 500.00m,
+                Type = TransactionTypeEnum.Variable,
+                CategoryId = Guid.NewGuid(),
+                EntryDate = DateTime.Now
+            };
+
+            // Act
+            receita.Description = "Receita Atualizada";
+            receita.Amount = 600.00m;
+
+            // Assert
+            Assert.Equal("Receita Atualizada", receita.Description);
+            Assert.Equal(600.00m, receita.Amount);
+        }
+
+        [Fact]
+        public void ExcluirReceitaFixa_EntradaValida_DeveExcluirComSucesso()
+        {
+            // Arrange
+            var receita = new Income
+            {
+                Description = "Receita Teste",
+                Amount = 500.00m,
+                Type = TransactionTypeEnum.Fix,
+                CategoryId = Guid.NewGuid(),
+                EntryDate = DateTime.Now
+            };
+
+            // Act
+            receita = null;
+
+            // Assert
+            Assert.Null(receita);
+        }
+
+        [Fact]
+        public void ExcluirReceitaVariavel_EntradaValida_DeveExcluirComSucesso()
+        {
+            // Arrange
+            var receita = new Income
+            {
+                Description = "Receita Teste",
+                Amount = 500.00m,
+                Type = TransactionTypeEnum.Variable,
+                CategoryId = Guid.NewGuid(),
+                EntryDate = DateTime.Now
+            };
+
+            // Act
+            receita = null;
+
+            // Assert
+            Assert.Null(receita);
+        }
+    }
+}
